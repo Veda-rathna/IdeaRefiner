c258660138fedbcfc04b8b5e1292ab0cf48273ae
Veda-rathna <vedarathna057@gmail.com>
2025-07-28 20:44:02 +0530

    Add environment variable support and enhance UI for idea generation and news scraping templates

--- Idea_generation/settings.py
+++ Idea_generation/settings.py
@@ -10,9 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialize environment variables
+env = environ.Env()
+# Read .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,3 +139,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Google API Key
+GOOGLE_API_KEY = env('GOOGLE_API_KEY', default=None)
